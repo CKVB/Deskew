bd8f8b390db05f075f72190f4c1ab39647a57e37
Chaitanya Krishna VB <avagadro2016@outlook.com>
2021-06-21 18:37:20 +0530

    added local file support

--- deskewImageAppProject/deskewImageApp/routes/deskewImage.py
+++ deskewImageAppProject/deskewImageApp/routes/deskewImage.py
@@ -12,43 +12,62 @@ def deskew_image():
 
     data = request.get_json()
     url = data["image_path"]
-    
-    response = requests.get(url, stream=True).raw
-    
-    image = np.asarray(bytearray(response.read()), dtype="uint8")
-    image = cv2.imdecode(image, cv2.IMREAD_COLOR)
-    image = np.array(image)
-    
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    
-    edges = cv2.Canny(gray, 50, 150, apertureSize=3)
 
-    width = 1000
+    if "http" in url:
+        try:
+            response = requests.get(url, stream=True).raw
+        except Exception:
+            return jsonify({"message": "invalid url."}), 400
+        else:
+            if response.status == 404:
+                return jsonify({"message": "file not found."}), 404
+            image = np.asarray(bytearray(response.read()), dtype="uint8")
+            image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+            image = np.array(image)
+    else:
+        image_file_path = os.path.isfile(url)
+        if image_file_path:
+            image = cv2.imread(url)
+        else:
+            return jsonify({"message": "file not found."}), 404
+
+    try:
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    except Exception:
+        return jsonify({"message": "request not processable."}), 422
+    else:
+        edges = cv2.Canny(gray, 50, 200, apertureSize=3)
+
+        width = 1000
+
+        lines = cv2.HoughLines(edges, 1, np.pi / 180, 50)
+
+        r, theta = lines[0][0]
+
+        x0 = int(r * np.cos(theta))
+        y0 = int(r * np.sin(theta))
+
+        x1 = int(x0 + width * (np.sin(theta)))
+        y1 = int(y0 - width * (np.cos(theta)))
+
+    try:
+        slope = (y1 - y0) / (x1 - x0)
+    except Exception:
+        return jsonify({"message": "not possible to deskew."}), 406
+    else:
+        angle = slope * (180 / np.pi)
     
-    lines = cv2.HoughLines(edges, 1, np.pi / 180, 50)
+        # cv2.line(image, (x0, y0), (x1, y1), (0, 255, 0), 2)
 
-    r, theta = lines[0][0]
-    
-    x0 = int(r * np.cos(theta))
-    y0 = int(r * np.sin(theta))
+        height, width, channel = image.shape
+        center = (width // 2, height // 2)
 
-    x1 = int(x0 + width * (np.sin(theta)))
-    y1 = int(y0 - width * (np.cos(theta)))
+        rotational_matrix = cv2.getRotationMatrix2D(center, angle, scale=1)
+        rotated_image = cv2.warpAffine(image, rotational_matrix, (width, height), borderMode=cv2.BORDER_REPLICATE)
 
-    slope = (y1 - y0) / (x1 - x0)
+        image_path = os.path.join(APP_ROOT, "static", "image.jpg")
+        cv2.imwrite(image_path, rotated_image)
 
-    angle = slope * (180 / np.pi)
+        deskewed_image_url = request.host_url + "static/image.jpg"
 
-    #cv2.line(image, (x0, y0), (x1, y1), (0, 255, 0), 2)
-
-    height, width, channel = image.shape
-    center = (width // 2, height // 2)
-    M = cv2.getRotationMatrix2D(center, angle, 1.0)
-    rotated = cv2.warpAffine(image, M, (width, height), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
-    
-    image_path = os.path.join(APP_ROOT, "static", "image.jpg")
-    cv2.imwrite(image_path, rotated)
-
-    deskewed_image_url = request.host_url + "static/image.jpg"
-    
-    return jsonify({"deskewed_image_url": deskewed_image_url, "skewed_angle": angle})
+        return jsonify({"deskewed_image_url": deskewed_image_url, "skewed_angle": angle}), 200
